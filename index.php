5220e8303bdc5486714f31f49ce5e21d775a6034
Fildadubcek <dubcekf@gmail.com>
2022-03-28 22:15:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
+    <title>Můj Web</title>
+</head>
+
+<body>
+    <div id="full_site">
+        <div id="header">
+            <div id="name">
+                <h1>Filip Dubček</h1>
+            </div>
+            <div id="menu">
+                <ul>
+                    <li><a>O mně</a></li>
+                    <li><a>Moje práce</a></li>
+                    <li><a>Spolupráce</a></li>
+                    <li><a>Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="info">
+            <h1>Ahoj! Já jsem Filip Dubček</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque laboriosam minima magni temporibus error, quae natus ex eaque libero consequuntur voluptates itaque vel, iure dolore dolorem. Tempore placeat laboriosam doloremque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas inventore accusantium veritatis illum illo officia aliquid tempore dolor ratione laboriosam, asperiores, eius corrupti! Quaerat laborum, atque eos aliquam voluptas aliquid!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eveniet cupiditate quas aliquam, praesentium asperiores ex tenetur ad est odio eum mollitia porro debitis assumenda repellat impedit magni sequi nulla.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia obcaecati quasi. Architecto officiis expedita quam? Quas dolorem magni qui vero eaque optio iusto ad exercitationem voluptates dicta, voluptatem ipsa!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione, ipsum et quisquam voluptatum quo. Consequuntur minus dignissimos laborum? Minima dolorem aperiam modi quam officia accusantium explicabo quae excepturi placeat?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit in dolores, ab molestias quis libero accusantium voluptatem fugiat. Magnam perspiciatis laudantium officiis sequi cum reprehenderit fugiat doloremque eligendi sapiente suscipit.</p>
+        </div>
+        <div id="footer">
+            <img src="/img/facebook.png" height="50px" width="60px">
+            <img src="/img/instagram.png" height="50px" width="70px">
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
