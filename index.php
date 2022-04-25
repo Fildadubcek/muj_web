b8d10a0322a82c8c70240870c8df7be362a63067
Fildadubcek <dubcekf@gmail.com>
2022-04-25 14:27:47 +0200

    muj_web

--- index.php
+++ index.php
@@ -11,42 +11,49 @@
 
 <body>
     <div id="full_site">
-        <div id="header">
-            <div id="menu">
-                <h1>Filip Dubček</h1>
-                <ul>
-                    <li><a href="/includes/index.php">O mně</a></li>
-                    <li><a href="/includes/work.php">Moje práce</a></li>
-                    <li><a href="/includes/together.php">Spolupráce</a></li>
-                    <li><a href="/includes/contacts.php">Kontakty</a></li>
-                </ul>
+        <div id="background">
+            <div id="header">
+                <div id="menu">
+                    <h1>FILIP DUBČEK</h1>
+                    <ul>
+                        <li><a href="/includes/index.php">O mně</a></li>
+                        <li><a href="/includes/work.php">Moje práce</a></li>
+                        <li><a href="/includes/together.php">Spolupráce</a></li>
+                        <li><a href="/includes/contacts.php">Kontakty</a></li>
+                    </ul>
+
+                </div>
+                <div class="clear">&nbsp;</div>
+            </div>
+            <div class="clear">
+               
+                <div id="info">
+                    <h1>I am Filip Dubček</h1>
+                    <h2>And I'm a Programmer</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque laboriosam minima magni temporibus error, quae natus ex eaque libero consequuntur voluptates itaque vel, iure dolore dolorem. Tempore placeat laboriosam doloremque!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas inventore accusantium veritatis illum illo officia aliquid tempore dolor ratione laboriosam, asperiores, eius corrupti! Quaerat laborum, atque eos aliquam voluptas aliquid!</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eveniet cupiditate quas aliquam, praesentium asperiores ex tenetur ad est odio eum mollitia porro debitis assumenda repellat impedit magni sequi nulla.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia obcaecati quasi. Architecto officiis expedita quam? Quas dolorem magni qui vero eaque optio iusto ad exercitationem voluptates dicta, voluptatem ipsa!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione, ipsum et quisquam voluptatum quo. Consequuntur minus dignissimos laborum? Minima dolorem aperiam modi quam officia accusantium explicabo quae excepturi placeat?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit in dolores, ab molestias quis libero accusantium voluptatem fugiat. Magnam perspiciatis laudantium officiis sequi cum reprehenderit fugiat doloremque eligendi sapiente suscipit.</p>
+                </div>
+            </div>
+            </div>
+
+
+
+            <div id="footer">
+                <div id="vlevo">
+                    <h1>Kde mě můžeš najít?</h1>
+                </div>
+                <div id="vpravo">
+                    <a href="https://www.facebook.com/filip.dubcek/" target="_blank"><img src="/img/facebook.png" height="50px" width="50px"></a>
+                    <a href="https://www.instagram.com/filda_dubcek/" target="_blank"><img src="/img/instagram.png" height="50px" width="50px"></a>
+                    <a href="https://www.tiktok.com/@kamenchytilvlak" target="_blank"><img src="/img/tiktok.png" height="50px" width="50px"></a>
+                </div>
 
             </div>
-            <div class="clear">&nbsp;</div>
-        </div>
-        <div class="clear">
-            <div id="image">
-                <img src="img/ja.jpg">
-            </div>
-            <div id="info">
-                <h1>Ahoj! Já jsem Filip Dubček</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque laboriosam minima magni temporibus error, quae natus ex eaque libero consequuntur voluptates itaque vel, iure dolore dolorem. Tempore placeat laboriosam doloremque!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas inventore accusantium veritatis illum illo officia aliquid tempore dolor ratione laboriosam, asperiores, eius corrupti! Quaerat laborum, atque eos aliquam voluptas aliquid!</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, eveniet cupiditate quas aliquam, praesentium asperiores ex tenetur ad est odio eum mollitia porro debitis assumenda repellat impedit magni sequi nulla.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia obcaecati quasi. Architecto officiis expedita quam? Quas dolorem magni qui vero eaque optio iusto ad exercitationem voluptates dicta, voluptatem ipsa!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ratione, ipsum et quisquam voluptatum quo. Consequuntur minus dignissimos laborum? Minima dolorem aperiam modi quam officia accusantium explicabo quae excepturi placeat?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit in dolores, ab molestias quis libero accusantium voluptatem fugiat. Magnam perspiciatis laudantium officiis sequi cum reprehenderit fugiat doloremque eligendi sapiente suscipit.</p>
-            </div>
-        </div>
-        <div id="footer">
-            <h1>Kde mě můžeš najít?</h1>
-            <a href="https://www.facebook.com/filip.dubcek/" target="_blank"><img src="/img/facebook.png" height="50px" width="50px"></a>
-            <a href="https://www.instagram.com/filda_dubcek/" target="_blank"><img src="/img/instagram.png" height="50px" width="50px"></a>
-            <a href="https://www.tiktok.com/@kamenchytilvlak" target="_blank"><img src="/img/tiktok.png" height="50px" width="50px"></a>
-        </div>
     </div>
-
-
 
 
 
